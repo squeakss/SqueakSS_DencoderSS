0b9041e9032fc776a7e6fa4bd383dbe29731c254
squeakss <86993447+squeakss@users.noreply.github.com>
2024-04-08 21:14:20 -0400

    formatting

--- src/main.rs
+++ src/main.rs
@@ -1,51 +1,51 @@
 use base64::{decode, encode};
-use md5::{compute};
-use std::io::{self, Write};
-use std::fs;
+use md5::compute;
 use rand::Rng;
-
-
+use std::fs;
+use std::io::{self, Write};
 
 fn main() {
-
-
-
-            println!("                _                       __");
-            println!("              /   \\                  /      \\ ");
-            println!("             '      \\              /          \\ ");
-            println!("            |       |Oo          o|            |");
-            println!("            `    \\  |OOOo......oOO|   /        |");
-            println!("             `    \\OOOOOOOOOOOOOOO\\//        /");
-            println!("               \\ _o\\OOOOOOOOOOOOOOOO//. ___ /");
-            println!("           ______OOOOOOOOOOOOOOOOOOOOOOOo.___");
-            println!("            --- OO'* `OOOOOOOOOO'*  `OOOOO--");
-            println!("                OO.   OOOOOOOOO'    .OOOOO o");
-            println!("                `OOOooOOOOOOOOOooooOOOOOO'OOOo");
-            println!("              .OO \"OOOOOOOOOOOOOOOOOOOO\"OOOOOOOo");
-            println!("          __ OOOOOO`OOOOOOOOOOOOOOOO\"OOOOOOOOOOOOo");
-            println!("         ___OOOOOOOO_\"OOOOOOOOOOO\"_OOOOOOOOOOOOOOOO");
-            println!("           OOOOO^OOOO0`(____)/\"OOOOOOOOOOOOO^OOOOOO");
-            println!("           OOOOO OO000/000000\000000OOOOOOOO OOOOOO");
-            println!("           OOOOO O0000000000000000 ppppoooooOOOOOO");
-            println!("           `OOOOO 0000000000000000 QQQQ \"OOOOOOO\"");
-            println!("            o\"OOOO 000000000000000oooooOOoooooooO'");
-            println!("            OOo\"OOOO.00000000000000000000OOOOOOOO'");
-            println!("           OOOOOO QQQQ 0000000000000000000OOOOOOO");
-            println!("          OOOOOO00eeee00000000000000000000OOOOOOOO.");
-            println!("         OOOOOOOO000000000000000000000000OOOOOOOOOO");
-            println!("         OOOOOOOOO00000000000000000000000OOOOOOOOOO");
-            println!("         `OOOOOOOOO000000000000000000000OOOOOOOOOOO");
-            println!("           \"OOOOOOOO0000000000000000000OOOOOOOOOOO'");
-            println!("             \"OOOOOOO00000000000000000OOOOOOOOOO\"");
-            println!("  .ooooOOOOOOOo\"OOOOOOO000000000000OOOOOOOOOOO\"");
-            println!(".OOO\"\"\"\"\"\"\"\"\"\".oOOOOOOOOOOOOOOOOOOOOOOOOOOOOo");
-            println!("OOO         QQQQO\"'         0II0         `\"QQQQ");
-            println!("OOO                          II");
-            println!("`OOo.                        II");
-            println!("  `\"OOOOOOOOOOOOoooooooo.    II");
-            println!("                             \\/");
-
-
+    println!("                              _                       __");
+    println!("                            /   \\                  /      \\ ");
+    println!("                           '      \\              /          \\ ");
+    println!("                          |       |Oo          o|            |");
+    println!("                          `    \\  |OOOo......oOO|   /        |");
+    println!("                           `    \\OOOOOOOOOOOOOOO\\//        /");
+    println!("                             \\ _o\\OOOOOOOOOOOOOOOO//. ___ /");
+    println!("                         ______OOOOOOOOOOOOOOOOOOOOOOOo.___");
+    println!("                          --- OO'* `OOOOOOOOOO'*  `OOOOO--");
+    println!("                              OO.   OOOOOOOOO'    .OOOOO o");
+    println!("                              `OOOooOOOOOOOOOooooOOOOOO'OOOo");
+    println!("                            .OO \"OOOOOOOOOOOOOOOOOOOO\"OOOOOOOo");
+    println!("                        __ OOOOOO`OOOOOOOOOOOOOOOO\"OOOOOOOOOOOOo");
+    println!("                       ___OOOOOOOO_\"OOOOOOOOOOO\"_OOOOOOOOOOOOOOOO");
+    println!("                         OOOOO^OOOO0`(____)/\"OOOOOOOOOOOOO^OOOOOO");
+    println!("                         OOOOO OO000/000000\000000OOOOOOOO OOOOOO");
+    println!("                         OOOOO O0000000000000000 ppppoooooOOOOOO");
+    println!("                         `OOOOO 0000000000000000 QQQQ \"OOOOOOO\"");
+    println!("                          o\"OOOO 000000000000000oooooOOoooooooO'");
+    println!("                          OOo\"OOOO.00000000000000000000OOOOOOOO'");
+    println!("                         OOOOOO QQQQ 0000000000000000000OOOOOOO");
+    println!("                        OOOOOO00eeee00000000000000000000OOOOOOOO.");
+    println!("                       OOOOOOOO000000000000000000000000OOOOOOOOOO");
+    println!("                       OOOOOOOOO00000000000000000000000OOOOOOOOOO");
+    println!("                       `OOOOOOOOO000000000000000000000OOOOOOOOOOO");
+    println!("                         \"OOOOOOOO0000000000000000000OOOOOOOOOOO'");
+    println!("                           \"OOOOOOO00000000000000000OOOOOOOOOO\"");
+    println!("                .ooooOOOOOOOo\"OOOOOOO000000000000OOOOOOOOOOO\"");
+    println!("              .OOO\"\"\"\"\"\"\"\"\"\".oOOOOOOOOOOOOOOOOOOOOOOOOOOOOo");
+    println!("              OOO         QQQQO\"'                      `\"QQQQ");
+    println!("              OOO                            ");
+    println!("              `OOo.                          ");
+    println!("                `\"OOOOOOOOOOOOoooooooo.      ");
+    println!("                                              ");
+    println!();
+    println!(" __                        _     __  __        ___                         _               ");
+    println!("/ _\\ __ _ _   _  ___  __ _| | __/ _\\/ _\\      /   \\___ _ __   ___ ___   __| | ___ _ __ ___ ");
+    println!("\\ \\ / _` | | | |/ _ \\/ _` | |/ /\\ \\ \\ \\      / /\\ / _ \\ '_ \\ / __/ _ \\ / _` |/ _ \\ '__/ __|");
+    println!("_\\ \\ (_| | |_| |  __/ (_| |   < _\\ \\_\\ \\    / /_//  __/ | | | (_| (_) | (_| |  __/ |  \\__ \\ ");
+    println!("\\__/\\__, |\\__,_|\\___|\\__,_|_|\\_\\__/\\__/___/___,' \\___|_| |_|\\___\\___/ \\__,_|\\___|_|  |___/");
+    println!("       |_|                            |_____|                                              ");
 
     loop {
         println!();
@@ -70,24 +70,25 @@ fn main() {
         println!();
 
         let mut choice = String::new();
-            print!("Enter the input type appended by the desired module:");
-            io::stdout().flush().unwrap(); // Ensure "Enter your choice: " is printed before reading input
-            io::stdin().read_line(&mut choice).expect("Big dumdum...");
+        print!("Enter the input type appended by the desired module:");
+        io::stdout().flush().unwrap(); // Ensure "Enter your choice: " is printed before reading input
+        io::stdin().read_line(&mut choice).expect("Big dumdum...");
 
-
-//Start matching
-//Start matching
-//Start matching
-//Start matching
-
+        //Start matching
+        //Start matching
+        //Start matching
+        //Start matching
 
         match choice.trim() {
             "1" => {
+                println!();
                 let input = read_input("Enter the ASCII string to encode to Base64: ");
                 let encoded = encode(input);
                 println_with_padding(&format!("Encoded: {}", encoded));
+                println!("___________________________________________________________________________________________");
             }
             "2" => {
+                println!();
                 let input = read_input("Enter the Base64 string to decode into ASCII: ");
                 match decode(&input) {
                     Ok(bytes) => match String::from_utf8(bytes) {
@@ -96,8 +97,10 @@ fn main() {
                     },
                     Err(_) => println_with_padding("Failed to decode Base64."),
                 }
+                println!("___________________________________________________________________________________________");
             }
             "3" => {
+                println!();
                 let input = read_input("Enter the ASCII string to encode to Binary: ");
                 let binary_encoded = input
                     .bytes()
@@ -105,64 +108,92 @@ fn main() {
                     .collect::<Vec<String>>()
                     .join(" ");
                 println_with_padding(&format!("Encoded to Binary: {}", binary_encoded));
+                println!("___________________________________________________________________________________________");
             }
             "4" => {
+                println!();
                 let input = read_input("Enter the Binary string to decode into ASCII: ");
                 match binary_to_ascii(&input) {
                     Ok(decoded) => println_with_padding(&format!("Decoded to ASCII: {}", decoded)),
                     Err(e) => println_with_padding(&format!("Error: {}", e)),
                 }
+                println!("___________________________________________________________________________________________");
             }
             "5" => {
+                println!();
                 let input = read_input("Enter the ASCII string to encode to Hex: ");
                 let hex_encoded = ascii_to_hex(&input);
                 println_with_padding(&format!("Encoded to Hex: {}", hex_encoded));
+                println!("___________________________________________________________________________________________");
             }
             "6" => {
+                println!();
                 let input = read_input("Enter the Hex string to decode into ASCII: ");
                 match hex_to_ascii(&input) {
                     Ok(decoded) => println_with_padding(&format!("Decoded to ASCII: {}", decoded)),
                     Err(e) => println_with_padding(&format!("Error: {}", e)),
                 }
+                println!("___________________________________________________________________________________________");
             }
             "7" => {
+                println!();
                 let input = read_input("Enter the Binary string to decode into Hex: ");
                 match binary_to_hex(&input) {
-                    Ok(hex_decoded) => println_with_padding(&format!("Decoded to Hex: {}", hex_decoded)),
+                    Ok(hex_decoded) => {
+                        println_with_padding(&format!("Decoded to Hex: {}", hex_decoded))
+                    }
                     Err(e) => println_with_padding(&format!("Error: {}", e)),
-    }
-}
+                }
+                println!("___________________________________________________________________________________________");
+            }
             "8" => {
+                println!();
                 let input = read_input("Enter the Hex string to encode to Binary: ");
                 match hex_to_binary(&input) {
-                    Ok(binary_encoded) => println_with_padding(&format!("Encoded to Binary: {}", binary_encoded)),
+                    Ok(binary_encoded) => {
+                        println_with_padding(&format!("Encoded to Binary: {}", binary_encoded))
+                    }
                     Err(e) => println_with_padding(&format!("Error: {}", e)),
-    }
-}
+                }
+                println!("___________________________________________________________________________________________");
+            }
             "MD5" => {
+                println!();
                 let input = read_input("Enter the string to hash with MD5: ");
                 let hash = create_md5_hash(&input);
                 println_with_padding(&format!("MD5 Hash: {}", hash));
-},
+                println!("___________________________________________________________________________________________");
+            }
             "Rot" => {
+                println!(); // You can keep this if you want to maintain the empty line before the prompt
                 let mut ciphertext = String::new();
-                println_with_padding("What's the string?");
+
+                // Adjusted to print on the same line and flush stdout
+                print!("What's the string? ");
+                io::stdout().flush().unwrap(); // Flush stdout to make sure the prompt is shown immediately
+
                 io::stdin()
-                .read_line(&mut ciphertext)
-                .expect("Failed to read line");
+                    .read_line(&mut ciphertext)
+                    .expect("Failed to read line");
+                println!();
                 ciphertext = ciphertext.trim_end().to_string();
 
                 for shift in 1..=25 {
-                let decrypted_message = Rotation(&ciphertext, shift);
-                println!("Shift {}: {}", shift, decrypted_message);
+                    let decrypted_message = Rotation(&ciphertext, shift);
+                    println!("Shift {}: {}", shift, decrypted_message);
+                    println!();
                 }
+                println!("___________________________________________________________________________________________");
             }
             "help" => {
+                println!();
                 println!("'s1' will convert an ASCII string to Base64");
                 println!("'f1' will read lines from a file and convert each ASCII line to Base64");
-                println!("'fMD5' will read lines from a file and run them through the MD5 hashing algorithm")
+                println!("'fMD5' will read lines from a file and run them through the MD5 hashing algorithm");
+                println!("___________________________________________________________________________________________");
             }
             "exit" => {
+                println!();
                 println_with_padding("Exiting...");
                 break;
             }
@@ -171,9 +202,6 @@ fn main() {
     }
 }
 
-
-
-
 //start of functions
 //start of functions
 //start of functions
@@ -181,10 +209,6 @@ fn main() {
 //start of functions
 //start of functions
 //start of functions
-
-
-
-
 
 fn read_input(prompt: &str) -> String {
     print!("{}", prompt);
@@ -222,20 +246,33 @@ fn hex_to_ascii(input: &str) -> Result<String, &'static str> {
 }
 
 fn binary_to_hex(input: &str) -> Result<String, &'static str> {
-    input.split(' ')
-         .map(|byte_str| u8::from_str_radix(byte_str, 2))
-         .collect::<Result<Vec<u8>, _>>()
-         .map_err(|_| "Invalid binary digit")
-         .map(|bytes| bytes.iter().map(|&b| format!("{:02x}", b)).collect::<Vec<String>>().join(" "))
+    input
+        .split(' ')
+        .map(|byte_str| u8::from_str_radix(byte_str, 2))
+        .collect::<Result<Vec<u8>, _>>()
+        .map_err(|_| "Invalid binary digit")
+        .map(|bytes| {
+            bytes
+                .iter()
+                .map(|&b| format!("{:02x}", b))
+                .collect::<Vec<String>>()
+                .join(" ")
+        })
 }
 
 fn hex_to_binary(input: &str) -> Result<String, &'static str> {
     (0..input.len())
         .step_by(3) // Considering there's a space between each hex byte representation.
-        .map(|i| u8::from_str_radix(&input[i..i+2], 16))
+        .map(|i| u8::from_str_radix(&input[i..i + 2], 16))
         .collect::<Result<Vec<u8>, _>>()
         .map_err(|_| "Invalid hex digit")
-        .map(|bytes| bytes.iter().map(|&b| format!("{:08b}", b)).collect::<Vec<String>>().join(" "))
+        .map(|bytes| {
+            bytes
+                .iter()
+                .map(|&b| format!("{:08b}", b))
+                .collect::<Vec<String>>()
+                .join(" ")
+        })
 }
 
 fn ascii_to_hex(input: &str) -> String {
